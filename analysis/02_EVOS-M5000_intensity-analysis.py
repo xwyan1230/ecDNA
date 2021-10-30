7c1f55ae1a0b273264f49cef15f97d9572e50b9b
Xiaowei Yan <xwyan@DERM-H4TG61HMPN7C.stanford.edu>
2021-10-29 19:00:22 -0700

    start writing the code, not finished yet

--- analysis/02_EVOS-M5000_intensity-analysis.py
+++ analysis/02_EVOS-M5000_intensity-analysis.py
@@ -1,9 +1,27 @@
-import matplotlib.pyplot as plt
+import os
+import numpy as np
 import napari
+import matplotlib.pyplot as plt
+from skimage.filters import try_all_threshold, threshold_otsu
+from skimage.measure import label, regionprops
+from skimage.morphology import binary_dilation, binary_erosion, disk, remove_small_objects
+from scipy import ndimage
 
-data_path = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20211022_ColoDM-Cas9_nucleofectionTest/"
-img = plt.imread('%s/SF-A1_0129_GFP.tif' % data_path, format=None)
+# input parameters
+exp_name = '20211022_EVOS-M5000_ColoDM-Cas9_nucleofectionTest'
+master_folder = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20211022_ColoDM-Cas9_nucleofectionTest/"
+data_source = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20211022_ColoDM-Cas9_nucleofectionTest/EVOS_M5000/"
+ch1 = 'GFP'  # channel for intensity measurement
+ch2 = 'TRANS'  # channel for segmentation, generally TRANS or BFP
 
-viewer = napari.Viewer()
-viewer.add_image(img)
-napari.run()
+files = [x for x in os.listdir("%s%s/" % (data_source, ch1))]
+
+for i in files:
+    img_ch1 = plt.imread('%s%s/%s' % (data_source, ch1, i))
+    img_ch2 = plt.imread('%s%s/%s%s.tif' % (data_source, ch2, i[:-(len(ch1)+4)], ch2))
+
+
+#viewer = napari.Viewer()
+#viewer.add_image(img_GFP)
+#viewer.add_image(img_trans)
+#napari.run()
