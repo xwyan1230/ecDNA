dedf8e3956834e98c91e366e0b99e0c764ed890b
Xiaowei Yan <xwyan@DERM-H4TG61HMPN7C.stanford.edu>
2021-11-17 15:12:39 -0800

    add list_unwrap function

--- shared/dataframe.py
+++ shared/dataframe.py
@@ -7,4 +7,24 @@ import random
 # FUNCTIONS for PD.DATAFRAME/LIST/UMANAGER/NUMBER
 # ---------------------------------------------------------------------------------------------------
 List related:
+
+    list_unwrap
+        FUNCTION: unwrap one layer of a list with first element per sublist
+        SYNTAX:   list_unwrap(lst: list)
 """
+
+
+def list_unwrap(lst: list):
+    """
+    Unwrap one layer of a list with first element per sublist
+
+    Examples:
+    input list: [[a1,a2],[b1,b2],[c1,c2],[d1,d2]]
+    output list: [a1,b1,c1,d1]
+    :param lst: list, the list to be unwrapped
+    :return: out: list
+    """
+    out = list()
+    for i in range(len(lst)):
+        out.append(lst[i][0])
+    return out
