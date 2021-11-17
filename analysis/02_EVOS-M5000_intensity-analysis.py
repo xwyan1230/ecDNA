1b08297992785b4c1e44b0b79116d0dd470e67b8
Xiaowei Yan <xwyan@DERM-H4TG61HMPN7C.stanford.edu>
2021-11-17 15:12:15 -0800

    finish this file for extracting intensity information from EVOS-M5000 acquired transfection images

--- analysis/02_EVOS-M5000_intensity-analysis.py
+++ analysis/02_EVOS-M5000_intensity-analysis.py
@@ -1,13 +1,12 @@
 import os
-import numpy as np
-import napari
+import pandas as pd
 import matplotlib.pyplot as plt
-from skimage.filters import try_all_threshold, threshold_otsu
-from skimage.measure import label, regionprops
-from skimage.morphology import binary_dilation, binary_erosion, disk, remove_small_objects
-from scipy import ndimage
+from skimage.measure import label
+from datetime import datetime
 import shared.segmentation as seg
 import shared.objects as obj
+import shared.dataframe as dat
+import napari
 
 # input parameters
 exp_name = '20211022_EVOS-M5000_ColoDM-Cas9_nucleofectionTest'
@@ -16,19 +15,53 @@ data_source = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20211022_ColoDM-C
 ch1 = 'GFP'  # channel for intensity measurement
 ch2 = 'TRANS'  # channel for segmentation, generally TRANS or BFP
 
+# write log file
+f = open("%slog.txt" % master_folder, "a+")
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+f.write("exp_name: %s\n" % exp_name)
+f.write("datetime: %s\n" % dt_string)
+f.write("code_running: %s\n" % __file__)
+f.write("master_folder: %s\n" % master_folder)
+f.write("data_source: %s\n" % data_source)
+f.write("Notes:\n")
+
+# get all the files to be analyzed
 files = [x for x in os.listdir("%s%s/" % (data_source, ch1))]
 
+# lists for creating data dataframe
+data_sample = list()
+data_int = list()
+
+f.write("going through data to extract intensity information...\n")
+count = 0
+
 for i in files:
+    print('# Analyzing %s ... (%d/%d)' % (i[:-(len(ch1)+5)], count+1, len(files)))
+
     # load images
     img_ch1 = plt.imread('%s%s/%s' % (data_source, ch1, i))[:, :, 1]
     img_ch2 = plt.imread('%s%s/%s%s.tif' % (data_source, ch2, i[:-(len(ch1)+4)], ch2))
-
+    # segment cells and get intensity information
     cell = seg.cell_seg_trans(img_ch2)
-    cell_int = obj.get_intensity(label(cell), [img_ch1])
+    cell_int = dat.list_unwrap(obj.get_intensity(label(cell), [img_ch1]))
+    # add data into lists
+    data_sample.append(i[:-(len(ch1)+5)])
+    data_int.append(cell_int)
 
+    count = count+1
 
+data = pd.DataFrame({'sample': data_sample, 'intensity': data_int})
+save_path = master_folder
+data.to_csv('%ssummary_intensity.txt' % save_path, index=False, sep='\t')
 
-#viewer = napari.Viewer()
-#viewer.add_image(img_GFP)
-#viewer.add_image(img_trans)
-#napari.run()
+print("DONE!")
+f.write("DONE!\n\n")
+f.close()
+
+"""
+viewer = napari.Viewer()
+viewer.add_image(img_GFP)
+viewer.add_image(img_trans)
+napari.run()
+"""
