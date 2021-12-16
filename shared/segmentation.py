1bbe779ec70d1f28db2f3ab95ba3d21feb2bf920
Xiaowei Yan <xwyan@DERM-H4TG61HMPN7C.stanford.edu>
2021-12-16 15:16:05 -0800

    add function to perform fluorescent image cell segmentation

--- shared/segmentation.py
+++ shared/segmentation.py
@@ -12,6 +12,11 @@ import shared.objects as obj
 cell_seg_trans
     FUNCTION: perform cell segmentation from a transmitted light image
     SYNTAX:   cell_seg_trans(img_trans: np.array, max_size=1000, min_size=100, selem_num=8)
+    
+cell_seg_fluorescent   
+    FUNCTION: perform cell segmentation from a fluorescent image
+    SYNTAX:   cell_seg_fluorescent(img: np.array, otsu_factor=1.5, maxima_threshold=1, max_size=1800, 
+                                   min_size=300, circ_thresh=0.6)
 
 """
 
@@ -51,6 +56,38 @@ def cell_seg_trans(img_trans: np.array, max_size=1000, min_size=100, selem_num=8
 
     out = obj.remove_large(out, max_size=max_size)
     out = obj.remove_small(out, min_size=min_size)
+
+    return out
+
+
+def cell_seg_fluorescent(img: np.array, otsu_factor=1.5, maxima_threshold=1, max_size=1800,
+                         min_size=300, circ_thresh=0.6):
+    """
+    Perform cell segmentation from a fluorescent image
+
+    Algorithm description:
+    Otsu segmentation followed by watershed and filtering
+
+    :param img: np.array
+                    fluorescent image
+    :param otsu_factor: float
+                    factor multiplied to otsu threshold used to perform otsu thresholding
+    :param maxima_threshold: int
+                    threshold for identify maxima during watershed
+    :param max_size: float
+                    maximum allowable object size
+    :param min_size: float
+                    minimum allowable object size
+    :param circ_thresh: float
+                    minimum allowable circularity
+    :return:
+    """
+    thresh_val = threshold_otsu(img)
+    out = img > thresh_val * otsu_factor
+    out = obj.label_watershed(out, maxima_threshold)
+    out = obj.label_remove_large(out, max_size)
+    out = obj.label_remove_small(out, min_size)
+    out = obj.label_remove_low_circ(out, circ_thresh)
 
     return out
 
