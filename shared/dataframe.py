a5034d22b0afc8bb2d63df05b4bcab926c02d7b8
Xiaowei Yan <xwyan@DERM-H4TG61HMPN7C.stanford.edu>
2022-02-25 15:13:18 -0800

    add more functions to facilitate ecDNA distance analysis

--- shared/dataframe.py
+++ shared/dataframe.py
@@ -15,6 +15,19 @@ List related:
     str_to_float
         FUNCTION: transform a string into a list of floats
         SYNTAX:   str_to_float(string: str)
+    
+    find_pos
+        FUNCTION: find the position of the first value in linearly increased list that is larger 
+                  than or equal to the given value
+        SYNTAX:   find_pos(value: int or float, increase_lst: list)
+    
+    mean_list
+        FUNCTION: calculate mean list from a list of lists, excluding 0 numbers
+        SYNTAX:   mean_list(lst: list)
+        
+    list_exclude_zero
+        FUNCTION: exclude zero from list y and corresponding index in x
+        SYNTAX:   list_exclude_zero(x: list, y: list)
 """
 
 
@@ -48,3 +61,71 @@ def str_to_float(string: str):
     """
     out = [float(i) for i in string[1:-1].split(', ')]
     return out
+
+
+def find_pos(value: int or float, increase_lst: list):
+    """
+    Find the position of the first value in linearly increased list that is larger than or equal to the
+    given value
+
+    Note: if value > max(increase_lst), which means such position does not exist in the given list. This
+        function will return len(increase_lst), which is the last position of the list + 1
+
+    Usage examples:
+    1) used to look for bleach frame
+       > bleach_frame = find_pos(bleach_time, time_tseries)
+
+    :param value: int or float
+    :param increase_lst: list, has to be a linearly increased list
+    :return: out: position of the first value in linearly increased list that is larger than or equal to
+                the given value, start from 0
+
+    """
+    out = len(increase_lst)-1
+    i = 0
+    while i < len(increase_lst):
+        if value <= increase_lst[i]:
+            out = i
+            break
+        else:
+            i += 1
+
+    return out
+
+
+def mean_list(lst: list):
+    """
+    Calculate mean list from a list of lists, excluding 0 numbers
+
+    :param lst: list, input list
+                [[...], [...], [...], [...],...[...]]
+    :return: out: list
+    """
+    out = []
+    for i in range(len(lst[0])):
+        n = 0
+        s = 0
+        for j in range(len(lst)):
+            if lst[j][i] != 0:
+                s = s + lst[j][i]
+                n = n + 1
+        out.append(s*1.0/n)
+
+    return out
+
+
+def list_exclude_zero(x: list, y: list):
+    """
+    Exclude zero from list y and corresponding index in x
+
+    :param x: list
+    :param y: list
+    :return:
+    """
+    x_out = []
+    y_out = []
+    for i in range(len(y)):
+        if y[i] != 0:
+            x_out.append(x[i])
+            y_out.append(y[i])
+    return x_out, y_out
