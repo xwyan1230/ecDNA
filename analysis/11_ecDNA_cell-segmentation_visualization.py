901d19c8d1129a8e0cf1acd5b2284dba2c61dc71
Xiaowei Yan <xwyan@DERM-H4TG61HMPN7C.stanford.edu>
2022-04-13 14:36:02 -0700

    2d analysis ecDNA continue

--- analysis/11_ecDNA_cell-segmentation_visualization.py
+++ analysis/11_ecDNA_cell-segmentation_visualization.py
@@ -1,29 +1,46 @@
 import matplotlib.pyplot as plt
-from skimage.measure import label, regionprops
+from skimage.measure import label, regionprops, regionprops_table
 import shared.segmentation as seg
 import shared.image as img
-from skimage.morphology import binary_dilation
+from skimage.morphology import binary_dilation, binary_erosion, disk
 import napari
+import pandas as pd
 
 # input parameters
-exp_name = '20220204_CtrlAndJQ1'
-master_folder = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20220204_CtrlAndJQ1/"
-prefix = 'ctrl_new_NW_20pos_Processed001'
+master_folder = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20220407_sp8_DMandHSR/HSR_singleZ/"
+prefix = '20220407_DMandHSR_HSR_singleZ'
 local_size = 100
 
-img_nuclear = plt.imread('%s%s/%s_s00_RAW_ch00.tif' % (master_folder, prefix, prefix), format=None)
-img_FISH = plt.imread('%s%s/%s_s00_RAW_ch01.tif' % (master_folder, prefix, prefix), format=None)
+img_nuclear = plt.imread('%s/%s_s5_ch00.tif' % (master_folder, prefix), format=None)
+img_FISH = plt.imread('%s/%s_s5_ch02.tif' % (master_folder, prefix), format=None)
 
-img_nuclear_seg = seg.nuclear_seg(img_nuclear)
-nuclear_props = regionprops(img_nuclear_seg)
+img_nuclear_seg = seg.nuclear_seg1(img_nuclear, local_factor=199, min_size=9000, max_size=40000)
+# 9000, 40000
+"""FISH_seg, img_FISH_seg = seg.find_organelle(img_FISH, 'na', extreme_val=18000, bg_val=seg.get_bg_int([img_FISH])[0], min_size=20, max_size=50000)
+img_FISH_seg = binary_dilation(img_FISH_seg, disk(8))
+for i in range(8):
+    img_FISH_seg = binary_erosion(img_FISH_seg)"""
+#img_FISH_seg = binary_erosion(img_FISH_seg, disk(8))
+"""for i in range(8):
+    img_FISH_seg = binary_dilation(img_FISH_seg)
+for i in range(8):
+    img_FISH_seg = binary_erosion(img_FISH_seg)"""
+props = regionprops(img_nuclear_seg)
+nuclear_props = pd.DataFrame(regionprops_table(img_nuclear_seg, img_nuclear,
+                                               properties=['label', 'area', 'convex_area', 'major_axis_length',
+                                                           'minor_axis_length', 'centroid', 'eccentricity',
+                                                           'max_intensity', 'min_intensity', 'mean_intensity',
+                                                           'solidity']))
 
+nuclear_props.to_csv('%stest.txt' % master_folder, index=False, sep='\t')
 viewer = napari.Viewer()
 viewer.add_image(img_nuclear)
 viewer.add_image(img_FISH)
 viewer.add_image(img_nuclear_seg)
+viewer.add_points([219, 481], size=10, face_color='red')
 napari.run()
 
-for i in range(len(nuclear_props)):
+"""for i in range(len(nuclear_props)):
     print("Start image analysis nuclear %s/%s..." % (i+1, len(nuclear_props)))
     original_centroid = nuclear_props[i].centroid
     position = img.img_local_position(img_nuclear_seg, original_centroid, local_size)
@@ -42,6 +59,6 @@ for i in range(len(nuclear_props)):
         viewer.add_image(FISH_seg)
         napari.run()
     else:
-        print("Nuclear does not pass convex filter.")
+        print("Nuclear does not pass convex filter.")"""
 
 print("END!")
---
+++ analysis/28_ecDNA_2d-analysis.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+from skimage.measure import label, regionprops, regionprops_table
+import shared.segmentation as seg
+import shared.image as img
+from skimage.morphology import binary_dilation, binary_erosion, disk, dilation
+import napari
+import shared.objects as obj
+from scipy import ndimage
+import pandas as pd
+
+# input parameters
+master_folder = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20220407_sp8_DMandHSR/DM_singleZ/"
+prefix = '20220407_DMandHSR_DM_singleZ'
+local_size = 100
+# load images
+img_nuclear = plt.imread('%s/%s_s0_ch00.tif' % (master_folder, prefix), format=None)
+img_FISH = plt.imread('%s/%s_s0_ch02.tif' % (master_folder, prefix), format=None)
+# nuclear segmentation
+img_nuclear_seg = seg.nuclear_seg1(img_nuclear, local_factor=99, min_size=9000, max_size=40000)
+nuclear_props = regionprops(img_nuclear_seg)
+
+if ctrl_nuclear_props[i].area*1.0/ctrl_nuclear_props[i].convex_area > 0.95:
+img_nuclear_seg_convex = seg.obj_to_convex(img_nuclear_seg)
+img_nuclear_seg_convex = dilation(img_nuclear_seg_convex, disk(3))
+
+
+# ecDNA segmentation
+_, img_FISH_seg = seg.find_organelle(img_FISH, 'na', extreme_val=18000, bg_val=seg.get_bg_int([img_FISH])[0],
+                                     min_size=5, max_size=50000)
+img_FISH_seg = binary_dilation(img_FISH_seg, disk(10))
+for i in range(10):
+    img_FISH_seg = binary_erosion(img_FISH_seg)
+img_FISH_seg = ndimage.binary_fill_holes(img_FISH_seg)
+img_FISH_seg = obj.remove_small(img_FISH_seg, min_size=20)
+img_FISH_seg[img_nuclear_seg_convex == 0] = 0
+# props
+
+
+viewer = napari.Viewer()
+viewer.add_image(img_nuclear_seg_convex, colormap='viridis', blending='additive')
+viewer.add_image(img_FISH_seg, blending='additive')
+napari.run()
+
+
+
